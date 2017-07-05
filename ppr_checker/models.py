01630bbf888de75c4c9b4db1c2f26fec64d7b5c8
student777 <darkdakku@gmail.com>
2017-07-05 18:29:38 +0900

    Add model

--- ppr_checker/models.py
+++ ppr_checker/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+
+class Good(models.Model):
+    name = models.CharField(max_length=45)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+
+class Deal(models.Model):
+    price = models.PositiveIntegerField()
+    good = models.ForeignKey(Good, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.good.name + str(self.price) + self.unit.name
